226966c1bbc204d3942177db174359d84cfa75df
Matheus <matheus007junior@hotmail.com>
2019-06-21 00:11:38 -0300

    Criando a estrutura bruta e sem estilo

---
+++ js/JavaScript1.js
@@ -0,0 +1,9 @@
+function Enviar() {
+
+  var nomevmid = document.getElementById("nomevmid");
+
+  if (nomevmid.value != "") {
+    alert('Obrigado sr(a) ' + nomevmid.value + ' os seus dados foram encaminhados com sucesso');
+  }
+
+}
