c4a742aa6ed0f57f4d5d9a9fd75666e5ba1d73e2
Matheus <matheus007junior@hotmail.com>
2019-06-22 21:58:35 -0300

    Melhorando um pouco o layout

--- js/JavaScript1.js
+++ js/JavaScript1.js
@@ -1,9 +1,27 @@
 function Enviar() {
 
   var nomevmid = document.getElementById("nomevmid");
+  var ipid = document.getElementById("ipid");
+  var ramid = document.getElementById("ramid");
+  var cpuid = document.getElementById("cpuid");
+
+  var name = nomevmid.value;
+  var cpu = cpuid.value;
+  var ram = ramid.value;
+  var ip = ipid.value;
 
   if (nomevmid.value != "") {
-    alert('Obrigado sr(a) ' + nomevmid.value + ' os seus dados foram encaminhados com sucesso');
+    alert('Obrigado sr(a) ' + ramid.value + ' os seus dados foram encaminhados com sucesso');
   }
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.open(
+    'GET',
+    `http://localhost:3000?cpu=${cpu}&ram=${ram}&ip=${ip}&name=${name}`,
+    true
+  );
+
+  httpRequest.send();
+
 
 }
+main();
